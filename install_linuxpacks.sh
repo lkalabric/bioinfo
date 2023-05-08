91000adf43c746c1314f1a326d0720e78bd367da
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-08 10:00:52 -0300

    Update install_linuxpacks.sh

--- install_linuxpacks.sh
+++ install_linuxpacks.sh
@@ -5,22 +5,22 @@
 # URL:
 # last update: 13 OUT 2021
 # Objetive: Install Ubuntu packages and keep record of all installations
-# Syntax: ./install_linuxpackages.sh <command>
+# Syntax: ./install_linuxpackages.sh <package>
 # Link: https://stackoverflow.com/questions/1298066/how-can-i-check-if-a-package-is-installed-and-install-it-if-not
 
-COMMAND=$1
+PACKAGE=$1
 HOME=/home/kalabric
 
 if [ $# = 0 ]; then
-	echo "Sintax error: ./install_linuxpackages.sh <command>"
+	echo "Sintax error: ./install_linuxpackages.sh <package>"
 	exit 0
 fi
 
-if ! which $COMMAND > /dev/null; then
-	echo -e "$COMMAND is not found! Install? (y/n) \c"
+if ! which $PACKAGE > /dev/null; then
+	echo -e "$PACKAGE is not found! Install? (y/n) \c"
 	read $REPLY
 	if [ $REPLY = "y" ]; then
-		sudo apt-get install ${COMMAND}
-		echo -ne "`date` sudo apt-get install $COMMAND\r" >> ${HOME}/scripts/install_linuxpackages.log
+		sudo apt-get install ${PACKAGE}
+		echo -ne "`date` sudo apt-get install $PACKAGE\r" >> ${HOME}/logs/install_linuxpackages.log
 	fi
 fi
