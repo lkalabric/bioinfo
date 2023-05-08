e494fc54b4cb73c042fceda5030e7fd634cb1e90
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-08 10:05:28 -0300

    Create header.sh

---
+++ header.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# autor: Luciano Kalabric Silva
+# institution: Oswaldo Cruz Foundation, Goncalo Moniz Institute, Bahia, Brazil
+# URL:
+# last update: 13 OUT 2021
+# Objetive: Install Ubuntu packages and keep record of all installations
+# Syntax: ./install_linuxpackages.sh <package>
+# Link: https://stackoverflow.com/questions/1298066/how-can-i-check-if-a-package-is-installed-and-install-it-if-not
