1514018c29a1f93c07d1c937539415caf01e95bd
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-08 10:57:53 -0300

    Update organize_filesystem.sh

--- organize_filesystem.sh
+++ organize_filesystem.sh
@@ -16,7 +16,7 @@ declare -a DIRECTORY_LIST=('bin' 'data' 'examples' 'logs' 'repos' 'results' 'scr
 cd ${HOME}
 
 # Read each directory name from the array and try to create a directory
-for DIRECTORY_NAME in ${DIRECTORY_LIST}; do 
+for DIRECTORY_NAME in ${DIRECTORY_LIST[@]}; do 
   [ -d $DIRECTORY_NAME ] && echo "Directory ${$DIRECTORY_NAME} exists!" || mkdir $DIRECTORY_NAME
 done
 
