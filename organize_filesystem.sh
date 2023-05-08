b448280dff89ce9f422f851d8bc0d36149fb9e94
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-08 14:13:18 -0300

    Update organize_filesystem.sh

--- organize_filesystem.sh
+++ organize_filesystem.sh
@@ -17,7 +17,7 @@ cd ${HOME}
 
 # Read each directory name from the array and try to create a directory
 for DIRECTORY_NAME in ${DIRECTORY_LIST[@]}; do 
-  [ -d ${DIRECTORY_NAME} ] && echo "Directory ${$DIRECTORY_NAME} exists!" || mkdir ${DIRECTORY_NAME}
+  [ -d ${DIRECTORY_NAME} ] && echo "Directory ${DIRECTORY_NAME} exists!" || mkdir ${DIRECTORY_NAME}
 done
 
 # Adding a directory to your PATH (temporary solution)
