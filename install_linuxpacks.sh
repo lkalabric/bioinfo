7968d3e2c34c67fa4580e781c8ea9fbc25978620
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-08 14:26:58 -0300

    Update install_linuxpacks.sh

--- install_linuxpacks.sh
+++ install_linuxpacks.sh
@@ -8,19 +8,20 @@
 # Syntax: ./install_linuxpackages.sh <package>
 # Link: https://stackoverflow.com/questions/1298066/how-can-i-check-if-a-package-is-installed-and-install-it-if-not
 
+
 PACKAGE=$1
-HOME=/home/kalabric
 
-if [ $# = 0 ]; then
-	echo "Sintax error: ./install_linuxpackages.sh <package>"
-	exit 0
-fi
-
-if ! which $PACKAGE > /dev/null; then
-	echo -e "$PACKAGE is not found! Install? (y/n) \c"
-	read $REPLY
-	if [ $REPLY = "y" ]; then
-		sudo apt-get install ${PACKAGE}
-		echo -ne "`date` sudo apt-get install $PACKAGE\r" >> ${HOME}/logs/install_linuxpackages.log
+while read -r line; do 
+	if [ $# = 0 ]; then
+		echo "Sintax error: ./install_linuxpackages.sh <package>"
+		exit 0
 	fi
-fi
+	if ! which $PACKAGE > /dev/null; then
+		echo -e "$PACKAGE is not found! Install? (y/n) \c"
+		read $REPLY
+		if [ $REPLY = "y" ]; then
+			sudo apt-get install ${PACKAGE}
+			echo -ne "`date` sudo apt-get install $PACKAGE\r" >> ${HOME}/logs/install_${PACKAGE}.log
+		fi
+	fi
+done < ${PACKAGE}
