32771434ffbf09f3969789ee307102206897a6ed
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-10 08:31:29 -0300

    Update install_linuxpacks.sh

--- install_linuxpacks.sh
+++ install_linuxpacks.sh
@@ -9,27 +9,46 @@
 # Link: https://stackoverflow.com/questions/1298066/how-can-i-check-if-a-package-is-installed-and-install-it-if-not
 
 # Linux packages listed in a file .packs
-PACKAGE_LIST=$1
 
 # Validate parameters
-if [ $# = 0 ]; then
-	echo "Sintax error: ./install_linuxpackages.sh <package_list .packs>"
-	exit 0
+case "$1" in
+	--help)
+		echo "Sintax: ./install_linuxpackages.sh <-i/-a> <package_list .packs>"
+	;;
+	-i)
+		echo "Installation in progress..."
+	;;
+	-a)
+		echo "Listing packages names and descrition..."
+esac
+exit 0
+
+if [ $1 = "--help" ]; then
+	echo "Sintax: ./install_linuxpackages.sh <package_list .packs>"
+	exit 1
 fi
 
-# Read package list and install linux command if not exists
+# If no parameter, assume all! 
+if [ $# = 0 ]; then
+	$PACKAGE_LIST="${HOME}/repos/biolinux/*.packs"
+	else
+	PACKAGE_LIST=$1
+fi
+
+# Read package list(s) and install linux command if not exists
 while IFS= read -r PACKAGE; do 
 	echo $PACKAGE
 	if ! which $PACKAGE > /dev/null; then
-		echo -e "$PACKAGE is not found! Install? (y/n) \c"
-		read -r
-		echo $REPLY
-		if [[ $REPLY = "y" ]]; then
+		echo -e "$PACKAGE is not found! Installation in progress..."
+		# echo -e "$PACKAGE is not found! Install? (y/n) \c"
+		# read -r
+		# echo $REPLY
+		#if [[ $REPLY = "y" ]]; then
 			sudo apt-get install ${PACKAGE}
 			echo -ne "`date` sudo apt-get install $PACKAGE\r" >> ${HOME}/logs/install_${PACKAGE}.log
-			else
-			echo "You can install it anytime!"
-		fi
+		#	else
+		#	echo "You can install it anytime!"
+		#fi
 		else
 		echo -e "$PACKAGE already installed in your Linux Distro!"
 	fi
