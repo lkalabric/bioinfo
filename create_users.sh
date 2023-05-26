778e1832543aa1d9b8c09e14a19506716aea3ad7
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-26 08:22:25 -0300

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+
 # Username
+# author: Luciano Kalabric Silva
+# institution: Oswaldo Cruz Foundation, Goncalo Moniz Institute, Bahia, Brazil
+# last update: 26 MAY 2023
+# objetive: Create users in Linux
+# syntax: ./create_users.sh <username>
+# link: https://linuxize.com/post/how-to-create-users-in-linux-using-the-useradd-command/
 
-# Link: https://linuxize.com/post/how-to-create-users-in-linux-using-the-useradd-command/
+# Edit file sudo nano /etc/default/useradd to SHELL=/bin/bash
 
-# Editar o arquivo /etc/default/useradd para configurar a variável SHELL=/bin/bash
-
+# Receive the username from the command line and store in a variable
 USERNAME = $1
+
+# Basic Linux commands
 sudo useradd $USERNAME
 sudo passwd $USERNAME
