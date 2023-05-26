9d20af4fb6d4182d9fc166d633456a098289b0c7
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-26 11:36:12 -0300

    Update git_scripts.sh

--- git_scripts.sh
+++ git_scripts.sh
@@ -1,5 +1,28 @@
-cd repos/bioinfo
-git pull
-cp *.sh ~/scripts/
-chmod +x ~/scripts/*
-cd
+#!/bin/bash
+
+# autor: Luciano Kalabric Silva
+# institution: Oswaldo Cruz Foundation, Goncalo Moniz Institute, Bahia, Brazil
+# last update: 26 MAY 2023
+# Objetive: Update scripts from a git repo
+# Syntax: ./git_scripts.sh <repo>
+# Link: 
+
+# Passes repo name to the script
+REPO=$1
+
+# Validate the parameter
+if [ $# = 0 ]; then
+    echo "Repository name required! Sintax: git_scripts.sh <repository>"  
+	  exit 0;
+	else
+  if [[ ! -d ${HOME}/repos/${REPO} ]]; then
+    echo "Repository not present in repos/"
+  else
+    cd ${HOME}/repos/$REPO
+    git pull
+    cp *.sh ~/scripts/
+    chmod +x ~/scripts/*
+    cd
+  fi
+fi
+
