200bb89542f7e3889c941b266feef203b3774298
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-26 08:18:42 -0300

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,4 +1,9 @@
 # Username
+
+# Link: https://linuxize.com/post/how-to-create-users-in-linux-using-the-useradd-command/
+
+# Editar o arquivo /etc/default/useradd para configurar a variável SHELL=/bin/bash
+
 USERNAME = $1
-sudo useradd USERNAME
-sudo passwd USERNAME
+sudo useradd $USERNAME
+sudo passwd $USERNAME
