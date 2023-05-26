f8053b2344507af28f049c3eb4b5d94a08cc0056
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-26 11:38:43 -0300

    Update git_scripts.sh

--- git_scripts.sh
+++ git_scripts.sh
@@ -13,16 +13,16 @@ REPO=$1
 # Validate the parameter
 if [ $# = 0 ]; then
     echo "Repository name required! Sintax: git_scripts.sh <repository>"  
-	  exit 0;
+    exit 0;
+else
+	if [[ ! -d ${HOME}/repos/${REPO} ]]; then
+	    echo "Repository not present in repos/"
 	else
-  if [[ ! -d ${HOME}/repos/${REPO} ]]; then
-    echo "Repository not present in repos/"
-  else
-    cd ${HOME}/repos/$REPO
-    git pull
-    cp *.sh ~/scripts/
-    chmod +x ~/scripts/*
-    cd
-  fi
+	    cd ${HOME}/repos/${REPO}
+	    git pull
+	    cp *.sh ~/scripts/
+	    chmod +x ~/scripts/*
+	    cd
+	fi
 fi
 
