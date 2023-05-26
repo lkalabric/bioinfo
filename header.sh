72481ae4053598ec31101472f3d64286ffb32e9c
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-26 08:36:01 -0300

    Update header.sh

--- header.sh
+++ header.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# autor: Luciano Kalabric Silva
+# command name: header.sh
+# author: Luciano Kalabric Silva
 # institution: Oswaldo Cruz Foundation, Goncalo Moniz Institute, Bahia, Brazil
-# URL:
 # last update: 13 OUT 2021
-# Objetive: Install Ubuntu packages and keep record of all installations
-# Syntax: ./install_linuxpackages.sh <package>
-# Link: https://stackoverflow.com/questions/1298066/how-can-i-check-if-a-package-is-installed-and-install-it-if-not
+# objetive: Header example
+# Syntax: ./header.sh <parameters>
+# Link: URL describing how to do...
