07e18ee151cb2c5bafa6a6fb29811ec9d3686833
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-05-26 11:52:03 -0300

    Create quality_control.sh

---
+++ quality_control.sh
@@ -0,0 +1,11 @@
+# There are different alternatives to this bioinfo step. Let's see some of them: 
+
+# 1) Fastqc
+# Link: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
+# Requirements: Java
+# Installation:
+# $ sudo apt install default-jre
+
+# 2) Fastqrc 
+# Link: https://rpkgs.datanovia.com/fastqcr/index.html
+# Requirements: R
