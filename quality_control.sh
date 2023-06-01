f476a9cab018847a77e511558cf6eba5646549fa
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-06-01 10:51:22 -0300

    Update quality_control.sh

--- quality_control.sh
+++ quality_control.sh
@@ -1,4 +1,4 @@
-# There are different alternatives to this bioinfo step. Let's see some of them: 
+# There are different alternatives methods in bioinfo to do the quality control. Let's see them both: 
 
 # 1) Fastqc
 # Link: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
@@ -9,3 +9,8 @@
 # 2) Fastqrc 
 # Link: https://rpkgs.datanovia.com/fastqcr/index.html
 # Requirements: R
+
+# 3) Software favorito de Gilmar
+# Link:
+# Requirements:
+
