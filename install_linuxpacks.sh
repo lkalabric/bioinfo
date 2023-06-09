98e581d97a472b00f8802e7b30833ab29b4e4300
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-06-09 12:00:25 -0300

    Update install_linuxpacks.sh

--- install_linuxpacks.sh
+++ install_linuxpacks.sh
@@ -15,7 +15,7 @@ if [[ $(sudo -v) ]]; then
 fi
 
 # Linux packages listed in a file .packs
-PACKAGELIST_DIR="${HOME}/repos/bioinfo"
+PACKAGELIST_DIR="${HOME}/repos/bioinfo/"
 if [[ -z $2 ]]; then
 	echo "Package name or package list *.packs file is required!"
 	echo "Syntax: ./install_linuxpacks.sh <-i/-l> <package name/package list *.packs file>"
