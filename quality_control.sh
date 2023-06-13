0f7d0e6eb362eed78d4c343bb2dc2e7df184b717
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-06-13 13:22:15 -0300

    Update quality_control.sh

--- quality_control.sh
+++ quality_control.sh
@@ -1,14 +1,23 @@
 # There are different alternatives methods in bioinfo to do the quality control. Let's see some of them: 
 
+# Example data
+SAMPLE="0001.1"
+INPUT_DIR="${HOME}/data/hbv/${SAMPLE}"
+OUTPUT_DIR="${HOME}/qc-results/${SAMPLE}"
+mkdir ${OUTPUT_DIR}
+cd ${OUTPUT_DIR}
+
+
+
 # 1) Fastqc
 # Link: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
 # Requirements: Java
-# $ sudo apt install default-jre
 # Installation:
+# $ sudo apt install default-jre
 # $ sudo apt install fastqc
 
 # Using example data
-fastqc -o qc-results -f fastq -c data/hbv/0001.1/*.fastq
+fastqc -o ${OUTPUT_DIR} -f fastq -c ${INPUT_DIR}/*.fastq
 
 # 2) Fastqrc 
 # Link: https://rpkgs.datanovia.com/fastqcr/index.html
@@ -18,10 +27,5 @@ fastqc -o qc-results -f fastq -c data/hbv/0001.1/*.fastq
 # 3) Afterqc
 # Link: https://github.com/OpenGene/AfterQC
 # Requirements: Miniconda (Python)
-INPUT_DIR="data/hbv"
-OUTPUT_DIR="qc-results"
-SAMPLE="0001.1"
-mkdir qc-results/$SAMPLE
-cd qc-results/$SAMPLE
 source activate afterqc
-after.py -1 $INPUT_DIR/$SAMPLE/*R1* -2 $INPUT_DIR/$SAMPLE/*R2*
+after.py -1 ${INPUT_DIR}/*R1* -2 ${INPUT_DIR}*R2*
