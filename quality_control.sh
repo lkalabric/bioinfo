94ba6dea13ada06ca1483ac3b744681d1809b119
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-06-13 10:41:01 -0300

    Update quality_control.sh

--- quality_control.sh
+++ quality_control.sh
@@ -3,15 +3,18 @@
 # 1) Fastqc
 # Link: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
 # Requirements: Java
-# Installation:
 # $ sudo apt install default-jre
+# Installation:
+# $ sudo apt install fastqc
+
+# Using example data
+fastqc -o qc-results -f data/hbv/0001.1/*.fastq
+
 
 # 2) Fastqrc 
 # Link: https://rpkgs.datanovia.com/fastqcr/index.html
 # Requirements: R
 
-# Using example data
-fastqc -o qc-results -f data/hbv/0001.1/*.fastq
 
 # 3) Software favorito de Gilmar
 # Link:
