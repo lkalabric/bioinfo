ef434ab65bd15b3dca4ada4186392080670c0683
Luciano Kalabric <luciano.kalabric@fiocruz.br>
2023-06-14 08:09:01 -0300

    Update quality_control.sh

--- quality_control.sh
+++ quality_control.sh
@@ -1,4 +1,14 @@
-# There are different alternatives methods in bioinfo to do the quality control. Let's see some of them: 
+#!/bin/bash
+
+# command name: quality_control.sh
+# author: Luciano Kalabric Silva
+# institution: Oswaldo Cruz Foundation, Goncalo Moniz Institute, Bahia, Brazil
+# last update: 14 JUN 2023
+# objetive: Give examples of quality control apps
+# Syntax: ./quality_control.sh
+# 
+
+# There are different methods in bioinfo to do the quality control. Let's see some of them: 
 
 # Example data
 SAMPLE="0001.1"
@@ -6,6 +16,8 @@ INPUT_DIR="${HOME}/data/hbv/${SAMPLE}"
 OUTPUT_DIR="${HOME}/qc-results/${SAMPLE}"
 [ -d ${OUTPUT_DIR} ] || mkdir ${OUTPUT_DIR}
 cd ${OUTPUT_DIR}
+
+# Preliminary data quality
 
 # 1) Fastqc
 # Link: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
@@ -28,3 +40,10 @@ fastqc -o ${OUTPUT_DIR} -f fastq -c ${INPUT_DIR}/*.fastq
 # $ install_thirdparty.sh
 source activate afterqc
 after.py -1 ${INPUT_DIR}/*R1* -2 ${INPUT_DIR}*R2*
+
+# Quality filtering
+
+# Use of timmomatic for Illumina data
+
+
+# Use of Nanofilt for MinIon data
