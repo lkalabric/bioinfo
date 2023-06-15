a7cdc3ca5751359c32e4b6eb762945b43b7753c9
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-06-15 11:00:12 -0300

    Create quality_filter.sh

---
+++ quality_filter.sh
@@ -0,0 +1,4 @@
+# 1) Trimmomatic
+# Use: Filtering and trimming Illumina data
+# Link: http://www.usadellab.org/cms/?page=trimmomatic
+java -jar trimmomatic-0.39.jar PE input_forward.fq.gz input_reverse.fq.gz output_forward_paired.fq.gz output_forward_unpaired.fq.gz output_reverse_paired.fq.gz output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36
