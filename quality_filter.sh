12b33b5519892a15a43d55a2934cf4e3f0167a3b
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-06-29 11:16:10 -0300

    Update quality_filter.sh

--- quality_filter.sh
+++ quality_filter.sh
@@ -7,45 +7,64 @@
 # objetive: Give examples of quality filter apps
 # Syntax: ./quality_filter.sh
 
-INPUTDIR="${HOME}/data/hbv/0001.1"
-OUTPUTDIR="${HOME}/qc-filter/hbv/0001.1"
+# Variable definitions
+INPUTDIR="${HOME}/data/hbv/$1"
+OUTPUTDIR="${HOME}/qc-filter/hbv/$1"
 [ ! -d ${OUTPUTDIR} ] && mkdir -p ${OUTPUTDIR}
+MINLENTGH=100
 
-##
-# Illumina data
-##
-# 1) Afterqc
-# Link: https://github.com/OpenGene/AfterQC
-# Requirements: Miniconda (Python)
-# Installation:
-# $ install_thirdparty.sh
-source activate afterqc
-# Single-ended analysis
-# after.py -d ${INPUT_DIR} -1 *R1*
-# after.py -d ${INPUT_DIR} -2 *R2*
-# Pair-ended analysis
-# after.py -d ${INPUT_DIR} -1 *R1* -2 *R2*
+# Validate arguments
+case $2 in
+  "-illumina")
+      ##
+    # Illumina data
+    ##
+    # 1) Afterqc
+    # Link: https://github.com/OpenGene/AfterQC
+    # Requirements: Miniconda (Python)
+    # Installation:
+    # $ install_thirdparty.sh
+    source activate afterqc
+    # Single-ended analysis
+    # after.py -d ${INPUT_DIR} -1 *R1*
+    # after.py -d ${INPUT_DIR} -2 *R2*
+    # Pair-ended analysis
+    # after.py -d ${INPUT_DIR} -1 *R1* -2 *R2*
+    
+    # 2) Trimmomatic
+    # Use: Filtering and trimming Illumina data
+    # Link: http://www.usadellab.org/cms/?page=trimmomatic
+    # java -jar trimmomatic-0.39.jar PE input_forward.fq.gz input_reverse.fq.gz output_forward_paired.fq.gz output_forward_unpaired.fq.gz output_reverse_paired.fq.gz output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36
+    source activate trimmomatic
+    trimmomatic PE ${INPUTDIR}/A24_S6_L001_R1_001.fastq.gz ${INPUTDIR}/A24_S6_L001_R2_001.fastq.gz ${OUTPUTDIR}/output_forward_paired.fq.gz ${OUTPUTDIR}/output_forward_unpaired.fq.gz ${OUTPUTDIR}/output_reverse_paired.fq.gz ${OUTPUTDIR}/output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:${MINLENTGH}
+;;
+  "-minion")
+    ##
+    # MinIon data
+    ##
+    # 1) Nanofilt
+    # Use: Filtering and trimming MinIon data
+    # Link: https://github.com/wdecoster/nanofilt
+    # Requirements: Miniconda (Python)
+    # Installation:
+    # $ install_thirdparty.sh
+    # Or: If you already have Conda env, run this
+    # $ conda create -n nanofilt
+    # $ source activate nanofilt
+    # $ conda install -c bioconda nanofilt
+    # source activate nanofilt
+    # NanoFilt -l 100 -q 9 ...
+    # The next step continues in other_filters.sh
+  ;;
+  *)
+  echo "Invalid parameter!"
+  exit 0
+esac
+  
 
-# 2) Trimmomatic
-# Use: Filtering and trimming Illumina data
-# Link: http://www.usadellab.org/cms/?page=trimmomatic
-# java -jar trimmomatic-0.39.jar PE input_forward.fq.gz input_reverse.fq.gz output_forward_paired.fq.gz output_forward_unpaired.fq.gz output_reverse_paired.fq.gz output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36
-source activate trimmomatic
-trimmomatic PE ${INPUTDIR}/A24_S6_L001_R1_001.fastq.gz ${INPUTDIR}/A24_S6_L001_R2_001.fastq.gz ${OUTPUTDIR}/output_forward_paired.fq.gz ${OUTPUTDIR}/output_forward_unpaired.fq.gz ${OUTPUTDIR}/output_reverse_paired.fq.gz ${OUTPUTDIR}/output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36
 
-##
-# MinIon data
-##
-# 1) Nanofilt
-# Use: Filtering and trimming MinIon data
-# Link: https://github.com/wdecoster/nanofilt
-# Requirements: Miniconda (Python)
-# Installation:
-# $ install_thirdparty.sh
-# Or: If you already have Conda env, run this
-# $ conda create -n nanofilt
-# $ source activate nanofilt
-# $ conda install -c bioconda nanofilt
-# source activate nanofilt
-# NanoFilt -l 100 -q 9 ...
-# The next step continues in other_filters.sh
+
+
+
+
+
