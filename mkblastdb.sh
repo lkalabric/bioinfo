a1554321c8d2e62aa3e0e42d8a41c82f09526bfb
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-08-03 10:23:06 -0300

    Create mkblastdb.sh

---
+++ mkblastdb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# command name: mkblastdb.sh
+# author: Luciano Kalabric Silva
+# institution: Oswaldo Cruz Foundation, Goncalo Moniz Institute, Bahia, Brazil
+# last update: 03 AGO 2023
+# objetive: Create blast dbs
+# Syntax: ./mkblastdb.sh <parameters>
+# Link: https://www.ncbi.nlm.nih.gov/books/NBK569841/
+
+# Syntax: ./mkblastdb.sh <path/dataset> <dbtype: nucl or prot> <dbname>
+
+# Define variables
+DATASET=$1
+DBTYPE=$2
+DBNAME=data/BLASTDB/$3
+
+# Makeblastdb
+makeblastdb -in $DATASET -parse_seqids -dbtype $DBTYPE -out $DBNAME
+
