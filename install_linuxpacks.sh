5b4d8bea17490492fc6ef47094ae1b3501ce0c7d
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-10-03 14:27:26 -0300

    Update install_linuxpacks.sh

--- install_linuxpacks.sh
+++ install_linuxpacks.sh
@@ -4,15 +4,16 @@
 # institution: Oswaldo Cruz Foundation, Goncalo Moniz Institute, Bahia, Brazil
 # URL:
 # last update: 13 OUT 2021
-# Objetive: Install Ubuntu packages and keep record of all installations
+# Objetive: Install Ubuntu packages and keep a record of all installations
 # Syntax: ./install_linuxpacks.sh <-i/-l> <package_name/package_list *.packs file>
 # Link: https://stackoverflow.com/questions/1298066/how-can-i-check-if-a-package-is-installed-and-install-it-if-not
 
-# This script is good to superuser or root user only!!!
+# This script is good for superuser or root user only!!!
 if [[ $(sudo -v) ]]; then
     sudo -v
     exit 0
 fi
+
 # Function to test if package exists in Debian
 function package_exists() {
     dpkg -s "$2" &> /dev/null
@@ -21,19 +22,19 @@ function package_exists() {
 
 # Linux packages are listed in files *.packs at the following $PACKAGELIST_DIR
 PACKAGELIST_DIR="${HOME}/repos/bioinfo"
-
+PACKAGELIST_FILENAME=$2
 
 # Validate parameters
 if [ $# = 0 ]; then
-	echo "Sintax: install_linuxpacks.sh <-i to install/-l to list> <package name or package list *.packs file>"
+	echo "Syntax: install_linuxpacks.sh <-i to install/-l to list> <package name or package list *.packs file>"
 	exit 0;
 else
-	if [[ -z $2 ]]; then
+	if [[ -z ${PACKAGELIST_FILENAME} ]]; then
 		echo "Package name or package list *.packs file is required!"
-		echo "Sintax: install_linuxpacks.sh <-i to install/-l to list> <package name or package list *.packs file>"
+		echo "Syntax: install_linuxpacks.sh <-i to install/-l to list> <package name or package list *.packs file>"
 		exit 0
 	else
- 		PACKAGE_LIST=$(cat ${PACKAGELIST_DIR}/$2)
+ 		PACKAGE_LIST=$(cat ${PACKAGELIST_DIR}/${PACKAGELIST_FILENAME})
 		case $1 in
 			"-i" ) echo "Installation in progress..."
 				# Pior to any installation it is recommended to update-upgrade your Linux Distro
@@ -49,12 +50,9 @@ else
 	    			done
 				exit 0
     			;;
-			* ) echo "Invalid option!"; exit 0 ;;
+			* ) echo "Invalid option!"; exit 0;;
 		esac	
-  		if [ -f ${PACKAGELIST_DIR}/$2 ]; then
-			PACKAGE_LIST=($(cat ${PACKAGELIST_DIR}/$2))
-		else
-			PACKAGE_LIST=$2
+  		PACKAGE_LIST=($(cat ${PACKAGELIST_DIR}/${PACKAGELIST_FILENAME}))
 			if ! package_exists ${PACKAGE_LIST}; then
 				echo "Package name wrong or package list *.packs not found!"
 				exit 0				
@@ -65,7 +63,7 @@ else
 	fi
 fi
 
-# Read package list and install each linux command if exists
+# Read the package list and install each Linux command if it exists
 for PACKAGE_NAME in "${PACKAGE_LIST[@]}"; do 	
 	if ! which $PACKAGE_NAME > /dev/null; then
 		echo -e "$PACKAGE_NAME is not installed! Install? (y/n) \c"
