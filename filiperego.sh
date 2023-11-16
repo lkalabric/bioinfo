fcffbf921f3fcc3998139c6ab73a1bec3f1eb004
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-11-16 12:16:30 -0300

    Update filiperego.sh

--- filiperego.sh
+++ filiperego.sh
@@ -12,9 +12,9 @@ TAIL_R1=1
 TAIL_R2=1
 
 cd ${USERNAME}/qc
-conda activate fastp
+source activate fastp
 fastp -i ${FILENAME}R1_001.fastq.gz -I ${FILENAME}R2_001.fastq.gz -o ${FILENAME}R1_trimmed.fastq.gz -O ${FILENAME}R2_trimmed.fastq.gz -q ${QUALITY} -l ${LENGTH} -f ${HEAD} -t ${TAIL_R1} -T ${TAIL_R2} -h 292879935.html
-conda deactivate
+source deactivate
 
 bwa index sars_cov_2_ref.fasta
 bwa mem sars_cov_2_ref.fasta ${FILENAME}R1_trimmed.gz ${FILENAME}R2_trimmed.gz | gzip -3 > aln-pe_${FILENAME}
