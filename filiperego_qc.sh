de22a614c72427a780ae658906dabf286f54f02d
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-11-16 14:25:05 -0300

    Update filiperego_qc.sh

--- filiperego_qc.sh
+++ filiperego_qc.sh
@@ -38,6 +38,8 @@ bwa index sars_cov_2_ref.fasta
 # Monta por referência das reads
 bwa mem sars_cov_2_ref.fasta ${RUNNAME}_R1_trimmed.fastq.gz ${RUNNAME}_R2_trimmed.fastq.gz | gzip -3 > aln-pe_${RUNNAME}
 
+exit 0
+
 # Gere a sequencia consenso
 #transforma o bam em sorted bam
 samtools view -bS aln-pe_${RUNNAME} | samtools sort - -o ${RUNNAME}.bam
