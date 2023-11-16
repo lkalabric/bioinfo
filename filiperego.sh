d64f8ed199d6e9c77f63c7fdc1e3493d3b6967cd
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-11-16 12:11:35 -0300

    Update filiperego.sh

--- filiperego.sh
+++ filiperego.sh
@@ -3,6 +3,7 @@ USERNAME=kalabric
 
 cp -r filiperego/qc/ ${USERNAME}/qc/
 
+FILENAME="292879835_S26_L001_"
 QUALITY=30
 LENTGH=50
 HEAD=14
@@ -11,8 +12,8 @@ TAIL_R2=1
 
 cd ${USERNAME}/qc
 conda activate fastp
-fastp -i 292879835_S26_L001_R1_001.fastq.gz -I 292879835_S26_L001_R2_001.fastq.gz -o 292879835_R1_trimmed.fastq.gz -O 292879835_R2_trimmed.fastq.gz -q ${QUALITY} -l ${LENGTH} -f ${HEAD} -t ${TAIL_R1} -T ${TAIL_R2} -h 292879935.html
+fastp -i ${FILENAME}R1_001.fastq.gz -I ${FILENAME}R2_001.fastq.gz -o ${FILENAME}R1_trimmed.fastq.gz -O ${FILENAME}R2_trimmed.fastq.gz -q ${QUALITY} -l ${LENGTH} -f ${HEAD} -t ${TAIL_R1} -T ${TAIL_R2} -h 292879935.html
 conda deactivate
 
-bwa index sars_cov_2.fasta
-bwa mem sars_cov_2_ref.fasta 292879835_R1_trimmed.gz 292879835_R2_trimmed.gz | gzip -3 > aln-pe_292879835
+bwa index sars_cov_2_ref.fasta
+bwa mem sars_cov_2_ref.fasta ${FILENAME}R1_trimmed.gz ${FILENAME}R2_trimmed.gz | gzip -3 > aln-pe_292879835
