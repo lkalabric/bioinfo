8fc01bc50283ff0c2cd6e57737216c740c71d564
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-08-20 11:50:01 -0300

    Update prot_mapping.sh

--- prot_mapping.sh
+++ prot_mapping.sh
@@ -2,6 +2,8 @@
 
 # Este script realiza o mapeamento de sequências de peptídeos em sequencias de proteínas utilizando o app peptidemapper
 EXAMPLES_DIR="${HOME}/examples/peptidemapper/"
+PROTEIN_FASTA=$1
+PEPTIDE_LIST=$2
 
 #Instalação do app
 #wget http://genesis.ugent.be/maven2/com/compomics/utilities/5.0.39/utilities-5.0.39.zip
@@ -9,5 +11,8 @@ EXAMPLES_DIR="${HOME}/examples/peptidemapper/"
 #cd utilities-5.0.39
 #java -cp utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p exampleFiles/PeptideMapping/yeast.fasta exampleFiles/PeptideMapping/yeast-pep-1k.csv results.csv
 
-# Uso do Peptide Mapper
+# Uso do Peptide Mapper (dados de exemplos)
 java -cp ~/bin/utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p ${EXAMPLES_DIR}/PeptideMapping/yeast.fasta ${EXAMPLES_DIR}/PeptideMapping/yeast-pep-1k.csv results.csv
+
+# Uso do Peptide Mapper (dados de exemplos)
+java -cp ~/bin/utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p ${PROTEIN_FASTA} ${PEPTIDE_LIST} results.csv
