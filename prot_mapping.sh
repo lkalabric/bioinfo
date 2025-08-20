13b0a6b75a2a8a997b4ca920a7d9cd7dea5f4129
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-08-20 12:44:13 -0300

    Update prot_mapping.sh

--- prot_mapping.sh
+++ prot_mapping.sh
@@ -12,7 +12,7 @@ PEPTIDE_LIST=$2
 #java -cp utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p exampleFiles/PeptideMapping/yeast.fasta exampleFiles/PeptideMapping/yeast-pep-1k.csv results.csv
 
 # Uso do Peptide Mapper (dados de exemplos)
-java -cp ${HOME}/bin/utilities-5.0.39/utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p ${EXAMPLES_DIR}/PeptideMapping/yeast.fasta ${EXAMPLES_DIR}/PeptideMapping/yeast-pep-1k.csv results.csv
+java -cp ${HOME}/bin/utilities-5.0.39/utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p ${EXAMPLES_DIR}/PeptideMapping/yeast.fasta ${EXAMPLES_DIR}/PeptideMapping/yeast-pep-1k.csv example_results.csv
 
 # Uso do Peptide Mapper (dados de exemplos)
 java -cp ${HOME}/bin/utilities-5.0.39/utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p ${PROTEIN_FASTA} ${PEPTIDE_LIST} results.csv
