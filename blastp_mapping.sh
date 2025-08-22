ba63d1c4c2675c2b8e7a1634b0eb23c57c4603b3
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-08-22 15:28:53 -0300

    Update blastp_mapping.sh

--- blastp_mapping.sh
+++ blastp_mapping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# script: blastanything.sh
+# script: blastp_mapping.sh
 # autores: Luciano Kalabric <luciano.kalabric@fiocruz.br>
 # instituição: Oswaldo Cruz Foundation, Gonçalo Moniz Institute, Bahia, Brazil
 # criação: 28 MAR 2023
@@ -24,13 +24,13 @@ BLASTDBDIR="${HOME}/data/BLASTDB/${BLASTDB}/"
 
 if [[ $# -lt 3 ]]; then
 	echo "Falta o nome do arquivo ou caminho contendo as queries, diretório BlastDB ou blast suite!"
-	echo "Sintáxe: ./blastanything.sh <QUERY: Path/File name.fasta> <BLASTDB> <BLASTSUITE: 'blastn', 'blastp', 'blastx', 'tblastn', 'tblastx'"
+	echo "Sintáxe: ./blastp_mapping.sh <QUERY: Path/File name.fasta> <BLASTDB> <BLASTSUITE: 'blastn', 'blastp', 'blastx', 'tblastn', 'tblastx'"
 	exit 0
 fi
 
 # Saída de dados
-BLASTRESULTSDIR="blast-results"
-[[ ! -d $BLASTRESULTSDIR ]] && mkdir $BLASTRESULTSDIR
+# BLASTRESULTSDIR="blast-results"
+# [[ ! -d $BLASTRESULTSDIR ]] && mkdir $BLASTRESULTSDIR
 
 # Preparação do BLASTDB local
 # Script: fasta2blastdb.sh
@@ -39,7 +39,7 @@ BLASTRESULTSDIR="blast-results"
 # A partir do arquivo blastdb.acc, cria o arquivo blastdb.map que mapeia os taxid (números que identificam as espécies taxonômica)
 
 # Declaração das funções do script
-function blastanything () {
+function blastp_mapping () {
   # Classificação taxonômica das reads utilizando blastn
   # Busca as QUERIES e salva na pasta diretório BLASTNREADSDIR
   # Parâmetros do Blast suite
@@ -55,7 +55,7 @@ function blastanything () {
 	# Busca as sequencias do arquivo query.fasta no banco de sequencias local
 	QUERYNAME=$(basename ${QUERY})
 		#CALL_FUNC=echo $(${BLASTSUITE} -db "${BLASTDBDIR}blastdb" -query "${QUERY}" -out "${BLASTRESULTSDIR}/${QUERYNAME}.${BLASTSUITE}" -outfmt "6 qseqid sseqid length mismatch gapopen qstart qend sstart send evalue qcovhsp" -qcov_hsp_perc ${QCOV} -max_target_seqs 1)	
-	   	CALL_FUNC=echo $(${BLASTSUITE} -db "${BLASTDBDIR}blastdb" -query "${QUERY}" -out "${BLASTRESULTSDIR}/${QUERYNAME}.${BLASTSUITE}.csv" -outfmt "10 qseqid sseqid sstart sseq" -qcov_hsp_perc ${QCOV} -max_target_seqs 1)	
+	   	CALL_FUNC=echo $(${BLASTSUITE} -db "${BLASTDBDIR}blastdb" -query "${QUERY}" -out "${QUERYNAME}.${BLASTSUITE}.csv" -outfmt "10 qseqid sseqid sstart sseq" -qcov_hsp_perc ${QCOV} -max_target_seqs 1)	
 	 	# CALL_FUNC=echo $(${BLASTSUITE} -db "${BLASTDBDIR}blastdb" -query "${QUERY}" -out "${BLASTRESULTSDIR}/${QUERYNAME}.${BLASTSUITE}" -outfmt "18" -qcov_hsp_perc ${QCOV} -max_target_seqs 1)
       	# Executa o comando contido na variável CALL_FUNC
       	eval $CALL_FUNC 
@@ -66,7 +66,7 @@ function blastanything () {
 	  for i in $(find ${QUERY}/*.fasta -type f -exec basename {} .fasta \; | sort); do
 		# Cria o comando Blast suite para busca em banco de sequencias local
 		# CALL_FUNC=echo$(${BLASTSUITE} -db "${BLASTDBDIR}blastdb" -query "${QUERY}${i}.fasta" -out "${BLASTRESULTSDIR}/${i}.${BLASTSUITE}" -outfmt "6 qseqid sseqid length mismatch gapopen qstart qend sstart send evalue qcovhsp" -qcov_hsp_perc ${QCOV} -max_target_seqs 1)
-  		CALL_FUNC=echo$(${BLASTSUITE} -db "${BLASTDBDIR}blastdb" -query "${QUERY}${i}.fasta" -out "${BLASTRESULTSDIR}/${i}.${BLASTSUITE}.csv" -outfmt "10 qseqid sseqid sstart sseq" -qcov_hsp_perc ${QCOV} -max_target_seqs 1)	
+  		CALL_FUNC=echo$(${BLASTSUITE} -db "${BLASTDBDIR}blastdb" -query "${QUERY}${i}.fasta" -out "${i}.${BLASTSUITE}.csv" -outfmt "10 qseqid sseqid sstart sseq" -qcov_hsp_perc ${QCOV} -max_target_seqs 1)	
 		# CALL_FUNC=echo$(${BLASTSUITE} -db "${BLASTDBDIR}blastdb" -query "${QUERY}${i}.fasta" -out "${BLASTRESULTSDIR}/${i}.${BLASTSUITE}" -outfmt "18" -qcov_hsp_perc ${QCOV} -max_target_seqs 1)
 		# Executa o comando contido na variável CALL_FUNC
 		eval $CALL_FUNC 
@@ -82,5 +82,5 @@ function blastanything () {
 # Main do script
 #
 # Executa a busca usando o Blast suite informado
-blastanything
+blastp_mapping
 exit 1
