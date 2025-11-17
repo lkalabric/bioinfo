759513443a631c12ca7486cf4663b96e10198992
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-11-17 06:36:28 -0300

    Add fastqc.sh to load configuration variables
    Script checks for a configuration file and loads variables.

---
+++ fastqc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Testando o carregamento de um arquivo de configuração
+CONFIG_FILE="./param/fastqc.param"
+if [ -f "$CONFIG_FILE" ]; then
+    # Carrega as variáveis do arquivo de configurações
+    source "$CONFIG_FILE" # . ./param/fastqc.param 
+else
+    echo "ERRO: Arquivo de configuração '$CONFIG_FILE' não encontrado."
+    exit 1
+fi
+echo "--- Carregamento de Variáveis Concluído ---"
+echo "Número de threads: $THREAD"
