0a3957fc7c6220d66d094ba30cb9ef73fd5aeabc
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2026-09-03 15:11:53 -0300

    Update peptidemapper.sh

--- peptidemapper.sh
+++ peptidemapper.sh
@@ -5,14 +5,22 @@ EXAMPLES_DIR="${HOME}/examples/peptidemapper/"
 PROTEIN_FASTA=$1
 PEPTIDE_LIST=$2
 
+
 #Instalação do app
 #wget http://genesis.ugent.be/maven2/com/compomics/utilities/5.0.39/utilities-5.0.39.zip
 #unzip utilities-5.0.39.zip
 #cd utilities-5.0.39
 #java -cp utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p exampleFiles/PeptideMapping/yeast.fasta exampleFiles/PeptideMapping/yeast-pep-1k.csv results.csv
 
-# Uso do Peptide Mapper (dados de exemplos)
-java -cp ${HOME}/bin/utilities-5.0.39/utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p ${EXAMPLES_DIR}/PeptideMapping/yeast.fasta ${EXAMPLES_DIR}/PeptideMapping/yeast-pep-1k.csv example_results.csv
+# Testa se PROTEIN_FASTA está vazia OU (-z) se PEPTIDE_LIST está vazia
+if [[ -z "$PROTEIN_FASTA" || -z "$PEPTIDE_LIST" ]]; then
+    echo "Sintaxe: peptidemapper proteoma.fasta peptideo.list"
+  # Uso do Peptide Mapper (dados de exemplos)
+  echo "Análise com dados de exemplos"
+  java -cp ${HOME}/bin/utilities-5.0.39/utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p ${EXAMPLES_DIR}/PeptideMapping/yeast.fasta ${EXAMPLES_DIR}/PeptideMapping/yeast-pep-1k.csv example_results.csv
+else
+  # Uso do Peptide Mapper (dados do usuário)
+  java -cp ${HOME}/bin/utilities-5.0.39/utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p ${PROTEIN_FASTA} ${PEPTIDE_LIST} results.csv
+fi
 
-# Uso do Peptide Mapper (dados de exemplos)
-java -cp ${HOME}/bin/utilities-5.0.39/utilities-5.0.39.jar com.compomics.cli.peptide_mapper.PeptideMapperCLI -p ${PROTEIN_FASTA} ${PEPTIDE_LIST} results.csv
+
